6b97e529622b43b5277d1a8bc7d1584cd48aec23
André Steiling <andre.steiling@icloud.com>
2016-08-05 09:35:59 +0200

    [FEATURE] SysCategory rendering for CE

--- Classes/ViewHelpers/DecodeJsonViewHelper.php
+++ Classes/ViewHelpers/DecodeJsonViewHelper.php
@@ -2,30 +2,17 @@
 namespace THREEME\Threeme\ViewHelpers;
 
 /*
- *  The MIT License (MIT)
+ * This file is part of the TYPO3 CMS project.
  *
- *  Copyright (c) 2016 André Steiling, http://www.threeme.de
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
+ * The TYPO3 project - inspiring people to share!
  */
-
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * @author André Steiling <andre.steiling@threeme.de>
@@ -33,12 +20,24 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class DecodeJsonViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
 {
 
-  /**
-   * Decodes a JSON string
-   * @param string $string
-   * @return array
-   */
-  public function render($string = '') {
-      return json_decode($string);
-  }
+    /**
+     * Initialize ViewHelper arguments
+     *
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('json', 'string', 'The JSON string which should be decoded to a array', true);
+        $this->registerArgument('assoc', 'boolean', 'When true, returned objects will be converted into associative arrays', false);
+    }
+
+    /**
+     * Render the view helper
+     *
+     * @return string
+     */
+    public function render() {
+        $cleanedJson = str_replace(array(',{', ',}', '[,{', '},]'), array('{', '}', '[{', '}]'), $json);
+        return json_decode($cleanedJson, $assoc);
+    }
 }
