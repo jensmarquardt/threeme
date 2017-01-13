f3b1a3f08f7e1096dff4ef4651b4b64a0d3161bb
André Steiling <andre.steiling@icloud.com>
2017-01-13 15:50:52 +0100

    [STYLE] BElogin color and background changes, fixed wrong var $extConfBackend

--- ext_tables.php
+++ ext_tables.php
@@ -19,13 +19,13 @@ if (TYPO3_MODE == 'BE') {
     // Add Threeme Logo and Background
     $extConfBackend = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
 
-    if (empty(trim($backendExtConf['loginLogo']))) {
+    if (empty(trim($extConfBackend['loginLogo']))) {
         $extConfBackend['loginLogo'] = 'EXT:threeme/Resources/Public/Images/Backend/LoginLogo.png';
     }
-    if (empty(trim($backendExtConf['loginBackgroundImage']))) {
-        $extConfBackend['loginHighlightColor'] = '#00629c';
+    if (empty(trim($extConfBackend['loginBackgroundImage']))) {
+        $extConfBackend['loginHighlightColor'] = '#166299';
     }
-    if (empty(trim($backendExtConf['loginBackgroundImage']))) {
+    if (empty(trim($extConfBackend['loginBackgroundImage']))) {
         $extConfBackend['loginBackgroundImage'] = 'EXT:threeme/Resources/Public/Images/Backend/LoginBackgroundImage.jpg';
     }
 
