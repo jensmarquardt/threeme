6d97a1f64d36dbfd07e6564f153811b33cedb53a
André Steiling <andre.steiling@icloud.com>
2017-05-22 17:15:23 +0200

    [TASK] Removed reintroduced "CSS Styled Content" content elements

--- Configuration/TSconfig/Backend/Page/mod.ts
+++ Configuration/TSconfig/Backend/Page/mod.ts
@@ -4,7 +4,7 @@ mod {
         defaultLanguageLabel = Deutsch
         defaultLanguageFlag = de
     }
-    
+
     web_layout {
         # By default, TYPO3 will not allow you to mix translated content and independent content in the page module.
         # Content elements violating this behavior will be marked in the Page Module and there is no UI control allowing you to create independent content elements in a given language.
@@ -13,6 +13,15 @@ mod {
         # Disable "QuickEdit" view
         menu.function {
             0 = 0
+        }
+    }
+
+    wizards {
+        newContentElement {
+            wizardItems {
+                # Removed reintroduced "CSS Styled Content" content elements
+                common.show := removeFromList(text,textpic,image)
+            }
         }
     }
 }
