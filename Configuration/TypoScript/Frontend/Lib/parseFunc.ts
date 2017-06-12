ccf8ebb35d3b6fa317cf23b32ebb5dbf2e7a05c8
André Steiling <andre.steiling@icloud.com>
2017-06-12 16:11:54 +0200

    [FIX] Enabled link icons in parseFunc (for CKEditor)

--- Configuration/TypoScript/Frontend/Lib/parseFunc.ts
+++ Configuration/TypoScript/Frontend/Lib/parseFunc.ts
@@ -6,7 +6,7 @@ lib.parseFunc {
                 ATagBeforeWrap = 1
                 wrap.prepend = TEXT
                 wrap.prepend {
-                    data = parameters:allParams
+                    data = parameters : allParams
                     listNum = 2
                     listNum.splitChar = 32
                     required = 1
@@ -16,6 +16,37 @@ lib.parseFunc {
                     replacement {
                         10.search = "
                         10.replace =
+                    }
+                }
+            }
+        }
+
+        a {
+            typolink {
+                # CK Editor Bug : https://forge.typo3.org/issues/80985
+                parameter.HTMLparser = 1
+                parameter.HTMLparser {
+                    htmlSpecialChars = -1
+                }
+
+                # Fonticons styled link classes
+                ATagBeforeWrap = 1
+                wrap.prepend = TEXT
+                wrap.prepend {
+                    data = parameters : allParams
+                    # parameters : allParams returns 'class="abc" href="url" ...'
+                    # Split by first space will return the full class attribute, we don't need to use 'listNum'
+                    #                    listNum = 2
+                    listNum.splitChar = 32
+                    required = 1
+                    wrap = <i class="fa threeme-ui-|" aria-hidden="true"></i>
+
+                    # Remove " and class attribute form wanted string
+                    replacement {
+                        10.search = "
+                        10.replace =
+                        20.search = class=
+                        20.replace =
                     }
                 }
             }
